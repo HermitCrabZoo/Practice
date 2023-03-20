de60bcbbfd2b6cb22c1c6467197208f5504b08c7
HermitCrabZoo <695863762@qq.com>
2023-03-20 17:08:37 +0800

    Java19 JEP427 demos

---
+++ src/main/java/com/zoo/java19/Jep427Demo.java
@@ -0,0 +1,23 @@
+package com.zoo.java19;
+
+/**
+ * Pattern Matching for switch
+ */
+public class Jep427Demo {
+    static boolean isLeapYear(int year) {
+        return (year % 400 == 0) || (year % 4 == 0 && year % 100 != 0);
+    }
+
+    static int getDaysOfMonth(int year, int month) {
+        return switch(month) {
+            case 2 -> isLeapYear(year) ? 29 : 28;
+            case 1, 3, 5, 7, 8, 10, 12 -> 31;
+            case 4, 6, 9, 11 -> 30;
+            default -> 0;
+        };
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getDaysOfMonth(2023, 2));
+    }
+}
